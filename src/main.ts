fbeefa693eb816df0a21709d3a8841a49b6ad6ad
Kiffin Gish <kiffin.gish@planet.nl>
2020-02-05 14:05:10 +0100

    ng e2e passes

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+/* tslint:disable:no-string-literal */
 import 'hammerjs';
 import './polyfills';
 
@@ -8,10 +9,10 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
   // Ensure Angular destroys itself on hot reloads.
-  if (window.ngRef) {
-    window.ngRef.destroy();
+  if (window['ngRef']) {
+    window['ngRef'].destroy();
   }
-  window.ngRef = ref;
+  window['ngRef'] = ref;
 
   // Otherwise, log the boot error
 }).catch(err => console.error(err));
