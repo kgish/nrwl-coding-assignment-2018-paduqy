aa8676b1bdef2c890c5c24fb2084e4ed56611437
Kiffin Gish <kiffin.gish@planet.nl>
2020-02-06 11:36:49 +0100

    Mock Backend test tickets component

--- src/app/pages/tickets/tickets.component.spec.ts
+++ src/app/pages/tickets/tickets.component.spec.ts
@@ -1,8 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Injectable } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
 
 import { MaterialModule } from '../../material/material.module';
 import { TicketsComponent } from './tickets.component';
+import { BackendService, Ticket } from '../../services/backend';
+
+const TICKETS: Ticket[] = [
+    { id: 1, description: 'Description #1', assigneeId: 1, completed: false },
+    { id: 2, description: 'Description #2', assigneeId: 2, completed: false },
+    { id: 3, description: 'Description #3', assigneeId: 3, completed: true },
+];
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MockBackendService {
+    constructor() {
+    }
+
+    tickets() {
+        return of(TICKETS);
+    }
+}
 
 describe('TicketsComponent', () => {
     let component: TicketsComponent;
@@ -11,7 +33,10 @@ describe('TicketsComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [ TicketsComponent ],
-            imports: [ MaterialModule, RouterTestingModule ]
+            imports: [ MaterialModule, RouterTestingModule ],
+            providers: [
+                { provide: BackendService, useClass: MockBackendService }
+            ]
         })
             .compileComponents();
     }));
@@ -29,5 +54,14 @@ describe('TicketsComponent', () => {
     it('should display correct page header', async(() => {
         const compiled = fixture.debugElement.nativeElement;
         expect(compiled.querySelector('.page-header').textContent).toContain('Tickets');
+    }));
+
+    it('should display correct number of tickets', async(() => {
+        expect(fixture.debugElement.queryAll(By.css('li')).length).toEqual(3);
+    }));
+
+    it('should display correct ticket descriptions', async(() => {
+        const compiled = fixture.debugElement.nativeElement;
+        [ 1, 2, 3 ].forEach(n => expect(compiled.querySelector(`li:nth-child(${n}) a`).textContent).toEqual(`Description #${n}`));
     }));
 });
